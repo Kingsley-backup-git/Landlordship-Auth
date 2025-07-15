cbeb3caa8f8268a75c76f30dc56f81834e7ab865
Okeze Kingsley <okezekingsley18@gmail.com>
2025-07-15 21:50:01 +0100

    user exp update

--- src/app/dashboard/(dashboard)/page.tsx
+++ src/app/dashboard/(dashboard)/page.tsx
@@ -6,10 +6,12 @@ import DashboardImage from "../../../../public/Overview22 1.svg"
 import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 import { PiUserPlusDuotone } from "react-icons/pi";
 import Image from 'next/image';
+import UserProvider from '@/app/components/Providers/UserProvider';
 
 export default function Dashboard() {
   return (
     <>
+    <UserProvider>
     <div className='sm:p-6 py-2 px-4 sm:block hidden sm:max-w-[960px] mx-auto w-[100%]'>
 <h1 className='font-semibold text-2xl text-[#1C1C1C]'>Welcome to YourLandlordship</h1>
 <p className='font-[400] text-[15px] text-[#1C1C1C] pt-1'>Getting started is very easy</p>
@@ -75,7 +77,10 @@ export default function Dashboard() {
 
 </div>
 </div>
+
+
 <MobileHome />
+</UserProvider>
     </>
   )
 }
